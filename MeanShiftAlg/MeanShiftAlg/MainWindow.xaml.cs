717f5b4478230faa11b96f11f4c0f57744769121
Mariusz Wójtowicz <Mariusz Wjtowicz>
2017-06-13 10:31:14 +0200

    Dodano GUI

--- MeanShiftAlg/MeanShiftAlg/MainWindow.xaml.cs
+++ MeanShiftAlg/MeanShiftAlg/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,58 @@ namespace MeanShiftAlg
     /// </summary>
     public partial class MainWindow : Window
     {
+        private BitmapImage imageSource;
         public MainWindow()
         {
             InitializeComponent();
+            FullImageRadio.Checked += FullImageRadio_Checked;
+            LocalWindowRadio.Checked += LocalWindowRadio_Checked;
+        }
+
+        private void LocalWindowRadio_Checked(object sender, RoutedEventArgs e)
+        {
+            LocalWindowParameters.Visibility = Visibility.Visible;
+        }
+
+        private void FullImageRadio_Checked(object sender, RoutedEventArgs e)
+        {
+            LocalWindowParameters.Visibility = Visibility.Collapsed;
+        }
+
+        private void LoadImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                imageSource = new BitmapImage(new Uri(openFileDialog.FileName));
+                BaseImage.Source = imageSource;
+            }
+        }
+
+        private void SegmentationButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool isLocalWindow = false;
+
+            if (imageSource != null && !string.IsNullOrWhiteSpace(ParameterHBox.Text))
+            {
+                if (LocalWindowRadio.IsChecked == true)
+                {
+                    if (!string.IsNullOrWhiteSpace(WindowHeightBox.Text)
+                        && !string.IsNullOrWhiteSpace(WindowWidthBox.Text))
+                    {
+                        var windowHeight = Convert.ToDouble(WindowHeightBox.Text);
+                        var windowWidth = Convert.ToDouble(WindowWidthBox.Text);
+                        isLocalWindow = true;
+                    }
+                    else
+                        isLocalWindow = false;
+                }
+                else
+                    isLocalWindow = false;
+
+                var image = imageSource;
+                var paramH = Convert.ToDouble(ParameterHBox.Text);
+            }
         }
     }
 }
